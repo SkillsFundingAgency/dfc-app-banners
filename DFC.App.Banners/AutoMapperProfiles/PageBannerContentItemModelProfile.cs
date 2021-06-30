1835a01eae547f5bcdbc94afad18c4ab13b7a18c
Ravi Mittal <ravi.mittal@methods.co.uk>
2021-06-30 15:47:36 +0100

    some changes for cache reload.

--- DFC.App.Banners/AutoMapperProfiles/PageBannerContentItemModelProfile.cs
+++ DFC.App.Banners/AutoMapperProfiles/PageBannerContentItemModelProfile.cs
@@ -2,6 +2,7 @@
 
 using AutoMapper;
 
+using DFC.App.Banners.Data.Models.CmsApiModels;
 using DFC.App.Banners.Data.Models.ContentModels;
 using DFC.App.Banners.ViewModels;
 
@@ -18,6 +19,18 @@ namespace DFC.App.Banners.AutoMapperProfiles
 
             CreateMap<BannerContentItemModel, BodyViewModel>()
                 .ForMember(d => d.Body, s => s.MapFrom(a => new HtmlString(a.Content)));
+
+            CreateMap<PageBannerContentItemApiDataModel, PageBannerContentItemModel>()
+                .ForMember(d => d.PartitionKey, s => s.Ignore())
+                .ForMember(d => d.PageLocation, s => s.Ignore())
+                .ForMember(d => d.PageName, s => s.Ignore())
+                .ForMember(d => d.Banners, s => s.Ignore())
+                .ForMember(d => d.LastReviewed, s => s.Ignore())
+                .ForMember(d => d.LastCached, s => s.Ignore())
+                .ForMember(d => d.Id, s => s.Ignore())
+                .ForMember(d => d.Etag, s => s.Ignore())
+                .ForMember(d => d.TraceId, s => s.Ignore())
+                .ForMember(d => d.ParentId, s => s.Ignore());
         }
     }
 }
