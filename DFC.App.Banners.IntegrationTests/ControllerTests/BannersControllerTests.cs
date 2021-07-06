976b2ed007b73be2f668c4da88a29d0cd0bfe313
Ravi Mittal <ravi.mittal@methods.co.uk>
2021-07-06 11:01:11 +0100

    integration tests project added.

---
+++ DFC.App.Banners.IntegrationTests/ControllerTests/BannersControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+
+using Xunit;
+
+namespace DFC.App.Banners.IntegrationTests.ControllerTests
+{
+    public class BannersControllerTests
+    {
+        private readonly CustomWebApplicationFactory<Startup> _factory;
+
+        public BannersControllerTests(CustomWebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task TestAsync()
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
