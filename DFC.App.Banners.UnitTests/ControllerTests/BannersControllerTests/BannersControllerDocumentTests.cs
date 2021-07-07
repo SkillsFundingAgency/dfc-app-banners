d3064740fd85711821a9681191f07cdda731ddd4
ravi-methods <83820221+ravi-methods@users.noreply.github.com>
2021-07-07 14:43:56 +0100

    Refactor and a couple of tests added for controller. (#10)
    * Minor refactor and tests.

--- DFC.App.Banners.UnitTests/ControllerTests/BannersControllerTests/BannersControllerDocumentTests.cs
+++ DFC.App.Banners.UnitTests/ControllerTests/BannersControllerTests/BannersControllerDocumentTests.cs
@@ -54,6 +54,22 @@ namespace DFC.App.Banners.UnitTests.ControllerTests.BannersControllerTests
 
         [Theory]
         [MemberData(nameof(HtmlMediaTypes))]
+        public async Task BannersControllerBodyHtmlReturnsNoContent(string mediaTypeName)
+        {
+            // Arrange
+            var controller = BuildBannersController(mediaTypeName);
+
+            // Act
+            var result = await controller.BodyAsync("/some-location");
+
+            // Assert
+            Assert.IsType<NoContentResult>(result);
+
+            controller.Dispose();
+        }
+
+        [Theory]
+        [MemberData(nameof(HtmlMediaTypes))]
         public async Task BannersControllerBodyHtmlReturnsSuccess(string mediaTypeName)
         {
             // Arrange
@@ -69,6 +85,31 @@ namespace DFC.App.Banners.UnitTests.ControllerTests.BannersControllerTests
             // Assert
             var viewResult = Assert.IsType<ViewResult>(result);
             _ = Assert.IsAssignableFrom<List<BannerViewModel>>(viewResult.ViewData.Model);
+
+            controller.Dispose();
+        }
+
+        [Theory]
+        [MemberData(nameof(HtmlMediaTypes))]
+        public async Task BannersControllerBodyHtmlWhenBannerNotFoundForPageLocationThenReturnsBannerForParentPageLocation(string mediaTypeName)
+        {
+            // Arrange
+            var path = "/careers-advice/interview-advice";
+            var expectedResults = A.CollectionOfDummy<PageBannerContentItemModel>(1);
+            expectedResults[0].Banners = new List<BannerContentItemModel>(A.CollectionOfDummy<BannerContentItemModel>(1));
+            var controller = BuildBannersController(mediaTypeName);
+            A.CallTo(() => FakeDocumentService.GetAsync(A<Expression<Func<PageBannerContentItemModel, bool>>>.Ignored))
+                .Returns(Array.Empty<PageBannerContentItemModel>()).Once()
+                .Then
+                .Returns(expectedResults);
+
+            // Act
+            var result = await controller.BodyAsync(path);
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            _ = Assert.IsAssignableFrom<List<BannerViewModel>>(viewResult.ViewData.Model);
+            A.CallTo(() => FakeDocumentService.GetAsync(A<Expression<Func<PageBannerContentItemModel, bool>>>.Ignored)).MustHaveHappenedTwiceExactly();
 
             controller.Dispose();
         }
