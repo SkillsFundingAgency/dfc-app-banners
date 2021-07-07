76a9989323a89c7741069e45aac1d5598fb4d40d
Prasad Ramireddy <pramireddy08@gmail.com>
2021-07-07 12:06:57 +0100

    Initial project setup for integration tests

---
+++ DFC.App.Banners.IntegrationTests/ControllerTests/WebhooksControllerRouteTests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Azure.EventGrid.Models;
+using System;
+using System.Net.Http;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DFC.App.Banners.IntegrationTests.ControllerTests
+{
+    [Trait("Category", "Integration")]
+    public class WebhooksControllerRouteTests : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private const string WebhookApiUrl = "/api/webhook/ReceiveEvents";
+
+        private readonly CustomWebApplicationFactory<Startup> factory;
+
+        public WebhooksControllerRouteTests(CustomWebApplicationFactory<Startup> factory)
+        {
+            this.factory = factory;
+        }
+
+        [Fact]
+        public async Task WebhooksControllerRouteTestsSubscriptionValidationReturnsSuccess()
+        {
+            // Arrange
+            string expectedValidationCode = Guid.NewGuid().ToString();
+            var eventGridEvents = BuildValidEventGridEvent(Microsoft.Azure.EventGrid.EventTypes.EventGridSubscriptionValidationEvent, new SubscriptionValidationEventData(expectedValidationCode, "https://somewhere.com"));
+            var uri = new Uri(WebhookApiUrl, UriKind.Relative);
+            var client = this.factory.CreateClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue(MediaTypeNames.Application.Json));
+
+            // Act
+            var response = await client.PostAsJsonAsync(uri, eventGridEvents).ConfigureAwait(false);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal($"{MediaTypeNames.Application.Json}; charset={Encoding.UTF8.WebName}", response.Content.Headers.ContentType.ToString());
+        }
+
+        private static EventGridEvent[] BuildValidEventGridEvent<TModel>(string eventType, TModel data)
+        {
+            var models = new EventGridEvent[]
+            {
+                new EventGridEvent
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Subject = "banners/an-integration-test-name",
+                    Data = data,
+                    EventType = eventType,
+                    EventTime = DateTime.Now,
+                    DataVersion = "1.0",
+                },
+            };
+
+            return models;
+        }
+    }
+}
