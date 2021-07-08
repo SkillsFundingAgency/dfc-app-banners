b2d877777c53c0428f9142dacd387485994c10dd
Prasad Ramireddy <pramireddy08@gmail.com>
2021-07-08 15:55:42 +0100

    Added intergration tests for BannersController

---
+++ DFC.App.Banners.IntegrationTests/ControllerTests/BannersControllerRouteTests.cs
@@ -0,0 +1,80 @@
+﻿using FluentAssertions;
+using System;
+using System.Net;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DFC.App.Banners.IntegrationTests.ControllerTests
+{
+    [Trait("Category", "Banners Controller Integration")]
+    public class BannersControllerRouteTests : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private readonly CustomWebApplicationFactory<Startup> factory;
+
+        public BannersControllerRouteTests(CustomWebApplicationFactory<Startup> factory)
+        {
+            this.factory = factory;
+        }
+
+        [Fact]
+        public async Task GetBannersReturnsSuccess()
+        {
+            // Arrange
+            var uri = new Uri("/", UriKind.Relative);
+            var client = this.factory.CreateClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue(MediaTypeNames.Text.Html));
+
+            // Act
+            var response = await client.GetAsync(uri).ConfigureAwait(false);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal($"{MediaTypeNames.Text.Html}; charset={Encoding.UTF8.WebName}", response.Content.Headers.ContentType.ToString());
+        }
+
+        [Theory]
+        [InlineData("/banners/document/careers-advice")]
+        [InlineData("/banners/document/reers-advice/career-choices-at-16")]
+        [InlineData("/banners/document/action-plans")]
+        [InlineData("/banners/document/contact-us")]
+        public async Task GetBannersDocumnetReturnsSuccess(string path)
+        {
+            // Arrange
+            var uri = new Uri(path, UriKind.Relative);
+            var client = this.factory.CreateClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue(MediaTypeNames.Text.Html));
+
+            // Act
+            var response = await client.GetAsync(uri).ConfigureAwait(false);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            response.StatusCode.Should().Be(HttpStatusCode.NoContent);
+        }
+
+        [Theory]
+        [InlineData("/banners/body/careers-advice")]
+        [InlineData("/banners/body/reers-advice/career-choices-at-16")]
+        [InlineData("/banners/body/action-plans")]
+        [InlineData("/banners/body/contact-us")]
+        public async Task GetBannersBodyReturnsSuccess(string path)
+        {
+            // Arrange
+            var uri = new Uri(path, UriKind.Relative);
+            var client = this.factory.CreateClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue(MediaTypeNames.Text.Html));
+
+            // Act
+            var response = await client.GetAsync(uri).ConfigureAwait(false);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            response.StatusCode.Should().Be(HttpStatusCode.NoContent);
+        }
+    }
+}
