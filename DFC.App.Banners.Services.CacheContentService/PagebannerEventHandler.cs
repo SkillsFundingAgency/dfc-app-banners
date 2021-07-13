d79052bbbffccde7436789bf6a3f09ee4fa55599
Prasad Ramireddy <pramireddy08@gmail.com>
2021-07-13 13:22:41 +0100

    Added code for bug FT2-787

--- DFC.App.Banners.Services.CacheContentService/PagebannerEventHandler.cs
+++ DFC.App.Banners.Services.CacheContentService/PagebannerEventHandler.cs
@@ -1,5 +1,6 @@
 ﻿using DFC.App.Banners.Data.Contracts;
 using DFC.App.Banners.Data.Helpers;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Net;
 using System.Threading.Tasks;
@@ -9,10 +10,14 @@ namespace DFC.App.Banners.Services.CacheContentService
     public class PagebannerEventHandler : IEventHandler
     {
         private readonly IWebhookContentProcessor webhookContentProcessor;
+        private readonly IBannerDocumentService bannerDocumentService;
+        private readonly ILogger<BannerEventHandler> logger;
 
-        public PagebannerEventHandler(IWebhookContentProcessor webhookContentProcessor)
+        public PagebannerEventHandler(IWebhookContentProcessor webhookContentProcessor, IBannerDocumentService bannerDocumentService, ILogger<BannerEventHandler> logger)
         {
             this.webhookContentProcessor = webhookContentProcessor;
+            this.bannerDocumentService = bannerDocumentService;
+            this.logger = logger;
         }
 
         public string ProcessType => CmsContentKeyHelper.PageBannerTag;
@@ -24,6 +29,14 @@ namespace DFC.App.Banners.Services.CacheContentService
 
         public async Task<HttpStatusCode> ProcessContentAsync(Guid contentId, Uri url)
         {
+            var pageBanner = await bannerDocumentService.GetByIdAsync(contentId);
+
+            if (pageBanner != null)
+            {
+                var deleteResponse = await bannerDocumentService.DeleteAsync(contentId);
+                logger.LogInformation($"Page Banner contentItem Id: {contentId}, result {deleteResponse}: Deleted content for Page Banner");
+            }
+
             return await webhookContentProcessor.ProcessContentAsync(url);
         }
     }
