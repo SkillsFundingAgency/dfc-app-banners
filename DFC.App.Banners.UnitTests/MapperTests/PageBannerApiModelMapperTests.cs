3f9929ce993fa2adf24009dbd73e3847961c2804
Ravi Mittal <ravi.mittal@methods.co.uk>
2021-07-15 16:11:28 +0100

    more changes.

--- DFC.App.Banners.UnitTests/MapperTests/PageBannerApiModelMapperTests.cs
+++ DFC.App.Banners.UnitTests/MapperTests/PageBannerApiModelMapperTests.cs
@@ -17,6 +17,7 @@ namespace DFC.App.Banners.UnitTests.MapperTests
     {
         [Theory]
         [InlineData("", "/")]
+        [InlineData("/", "/")]
         [InlineData("randompage", "/randompage")]
         [InlineData("randompage/", "/randompage")]
         [InlineData("/randompage", "/randompage")]
