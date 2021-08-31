83ea743c557431b5c68bdabcf78ac6eee286e7af
Prasad Ramireddy <pramireddy08@gmail.com>
2021-08-31 11:31:56 +0100

    minor change to resolve merge conflicts

--- DFC.App.Banners.IntegrationTests/ControllerTests/BannersControllerRouteTests.cs
+++ DFC.App.Banners.IntegrationTests/ControllerTests/BannersControllerRouteTests.cs
@@ -37,7 +37,7 @@ namespace DFC.App.Banners.IntegrationTests.ControllerTests
             client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue(MediaTypeNames.Text.Html));
 
             // Act
-            var response = await client.GetAsync(uri).ConfigureAwait(false);
+            var response = await client.GetAsync(uri);
 
             // Assert
             response.EnsureSuccessStatusCode();
@@ -55,7 +55,7 @@ namespace DFC.App.Banners.IntegrationTests.ControllerTests
             client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue(MediaTypeNames.Text.Html));
 
             // Act
-            var response = await client.GetAsync(uri).ConfigureAwait(false);
+            var response = await client.GetAsync(uri);
 
             // Assert
             response.EnsureSuccessStatusCode();
@@ -73,7 +73,7 @@ namespace DFC.App.Banners.IntegrationTests.ControllerTests
             client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue(MediaTypeNames.Text.Html));
 
             // Act
-            var response = await client.GetAsync(uri).ConfigureAwait(false);
+            var response = await client.GetAsync(uri);
 
             // Assert
             response.EnsureSuccessStatusCode();
